011219a27b4eface27cf072eb798a4f0e2f33c1d
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2023-09-16 11:01:00 +0530

    Add files via upload

---
+++ 29.08.2023/program1.c
@@ -0,0 +1,12 @@
+// wap to ascii value of characters.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char ch;
+   printf("\n Enter any character:");
+   scanf("%c",&ch);
+   printf("\n ASCII value of %c is %d",ch,ch);
+   
+
+}
---
+++ 29.08.2023/program2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i;
+   float f;
+   char c;
+   double d;
+   printf("\n Size of int: %d\n Size of float: %d\n Size of char: %d\n Size of double: %d",sizeof(i),sizeof(f),sizeof(c),sizeof(d));
+
+}
