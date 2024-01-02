b9ff4b037982f6034b4e6ed419312d571ba0912a
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2024-01-02 15:27:10 +0530

    Add files via upload

---
+++ cdac/Day 6-27.12.2023/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Test2
+    {
+        int[] age = { 12, 32, 14, 22 };
+
+        var a=from i in age where i>20 orderby i dec
+             select i;
+            foreach (String item in a){
+
+            }
+
+    }
+}
---
+++ cdac/Day 6-27.12.2023/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+                Console.WriteLine(c);
+            }catch(Exception e)
+            {
+                //Console.WriteLine("you got exception bro..!!!");
+                Console.WriteLine(e.ToString());
+            }
+            finally { Console.WriteLine("check your code bro!!"); }
+
+            Console.ReadLine();
+        }
+    }
+}
